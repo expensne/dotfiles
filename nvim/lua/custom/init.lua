3d7a2b11e74444a0f8aa747e4d8cbf611a032d8d
Fabian Grosch <fabian.grosch@datev.de>
2026-01-07 13:30:42 +0100

    add yank highlighting

--- nvim/lua/custom/init.lua
+++ nvim/lua/custom/init.lua
@@ -1,3 +1,14 @@
+-- Disable wrap
+vim.opt.wrap = false
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("YankHighlight", {}),
+  callback = function()
+    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+  end,
+})
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
   group = vim.api.nvim_create_augroup("YankHighlight", {}),
